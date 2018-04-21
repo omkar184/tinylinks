e46788aab5f44491774ef222e7b19a951c14b6f4
Omkar Sagare <forlancer01@gmail.com>
2018-04-21 16:23:06 +0530

    Update: Database file

---
+++ Docs/tinylinks.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 21, 2018 at 12:52 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tinylinks`
+--
+CREATE DATABASE IF NOT EXISTS `tinylinks` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `tinylinks`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_urls`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `long_url` text NOT NULL,
+  `short_url` varchar(250) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
